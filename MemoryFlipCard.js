fd7f588cd5a123e6eb74ca80af9bb67e7997bdf5
BohemianMoonchild <alexsarosa411@gmail.com>
2022-05-12 21:55:32 -0400

    dailycommit

--- MemoryFlipCard.js
+++ MemoryFlipCard.js
@@ -1,58 +1,139 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+
+
 // cards six in arrays
 const cardsArray = [
     {
         name:'children',
-        image: 'https://bl3302files.storage.live.com/y4mG52qhdArZOY_AFcik_m_NhIBDyALDIxTc_gLxdHIGQGpfNEHl3EaO7P4zHce25-nVlktzGLTdrgm0gITjuBmNm9ht6wmGInIijUsMZXI5Z1NXJaUi84w1tWZoUgSmQA3mewuTfG3mlQLxz1PH1HIumYWkb3oWeKtQ-5ACenT3VoYuHeUfiu0D5rJunSzKcO3?width=300&height=300&cropmode=none',  
+        image: 'IMAGES/Card2Children.jpg',  
     },
 
     {   name:'children',
-        image: 'https://bl3302files.storage.live.com/y4mG52qhdArZOY_AFcik_m_NhIBDyALDIxTc_gLxdHIGQGpfNEHl3EaO7P4zHce25-nVlktzGLTdrgm0gITjuBmNm9ht6wmGInIijUsMZXI5Z1NXJaUi84w1tWZoUgSmQA3mewuTfG3mlQLxz1PH1HIumYWkb3oWeKtQ-5ACenT3VoYuHeUfiu0D5rJunSzKcO3?width=300&height=300&cropmode=none',  
+        image: 'IMAGES/Card2Children.jpg',  
     },
 
     {   name: 'child1',
-        image: 'https://bl3302files.storage.live.com/y4mS-cDc-sKuV9UzHUZCwEvhzoiiXfwkqrfzVuGfGfyVPumgRMGmf4jcTj0cwaIuH_GA82bEghcbgr6t_PT5Z-Q49grt3kniHff_iZyQhM9gPNRwrV91Hdh4LcGYzPTGUTvipjYe3Yf62sifJC8QYIryZZ71Aje3ZGx32VcPzcesJBAON_yDJtG7tBWbhql5lYM?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card1Yvette.jpg',
     },
 
     {   name: 'child1',
-        image: 'https://bl3302files.storage.live.com/y4mS-cDc-sKuV9UzHUZCwEvhzoiiXfwkqrfzVuGfGfyVPumgRMGmf4jcTj0cwaIuH_GA82bEghcbgr6t_PT5Z-Q49grt3kniHff_iZyQhM9gPNRwrV91Hdh4LcGYzPTGUTvipjYe3Yf62sifJC8QYIryZZ71Aje3ZGx32VcPzcesJBAON_yDJtG7tBWbhql5lYM?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card1Yvette.jpg',
     },
 
     {   name: 'child2',
-        image: 'https://bl3302files.storage.live.com/y4mXi_sXQZxnRLl9UoI2pz6i7uFh7agsHYh5fyFVLK2pAsQcyffBzOp1YOAARGRaTwrxMnta2IUbRdCTIT9N0rYII06ZqDZhtn1KK042O7pa-sAHdIUNBdJArJMdXhdLzwwhShs2P37TDfdKH3omYgN8wz_WKNvyKMDPduILTX6ISFX3DPwftc9CTrM710vSIUk?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card5Alexsa.jpg',
     },
 
     {   name: 'child2',
-        image: 'https://bl3302files.storage.live.com/y4mXi_sXQZxnRLl9UoI2pz6i7uFh7agsHYh5fyFVLK2pAsQcyffBzOp1YOAARGRaTwrxMnta2IUbRdCTIT9N0rYII06ZqDZhtn1KK042O7pa-sAHdIUNBdJArJMdXhdLzwwhShs2P37TDfdKH3omYgN8wz_WKNvyKMDPduILTX6ISFX3DPwftc9CTrM710vSIUk?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card5Alexsa.jpg',
     },
 
     {   name: 'child3',
-        image: 'https://bl3302files.storage.live.com/y4mSo-V2C_ODFsIjAPhVrY7aNIvVRLAhOh6022dYX6Xyi0ZXpynBt5EidceEmPID-hy6o5xb7AHBL6oKiXGGhmBtQN7js86DNDaXyC2RdaXISEW6cJiWr_j9gepdrUNE2Y0KLjna6jCn0pFH56TPgUW6lxphe62AC2HOX__wcR6eZ4E6bvdWaSfCihJ-zCswNaX?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card6Junito.jpg',
     },
 
     {   name: 'child3',
-        image: 'https://bl3302files.storage.live.com/y4mSo-V2C_ODFsIjAPhVrY7aNIvVRLAhOh6022dYX6Xyi0ZXpynBt5EidceEmPID-hy6o5xb7AHBL6oKiXGGhmBtQN7js86DNDaXyC2RdaXISEW6cJiWr_j9gepdrUNE2Y0KLjna6jCn0pFH56TPgUW6lxphe62AC2HOX__wcR6eZ4E6bvdWaSfCihJ-zCswNaX?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card6Junito.jpg',
     },
 
     {   name: 'parents',
-        image: 'https://bl3302files.storage.live.com/y4mbOrvUQPtDakKeKQok3WuNkl28qaJCzerLwt_IJPHjjdfF2esPiUUpOtNDQuvJbwZnBhZjRB7LxDayjBTObpW9aSzSxq8qh5TR6IwlcoaPF6tURF3HwQ_PS_jscdFuvquaCqx_n_d68BjlaIzjQ5Ht621mztyt0wtQYqYu-h9L9QHNHS5-zzAfTc_1rtFSp_s?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card4Parents.jpg',
     },
 
     {   name: 'parents',
-        image: 'https://bl3302files.storage.live.com/y4mbOrvUQPtDakKeKQok3WuNkl28qaJCzerLwt_IJPHjjdfF2esPiUUpOtNDQuvJbwZnBhZjRB7LxDayjBTObpW9aSzSxq8qh5TR6IwlcoaPF6tURF3HwQ_PS_jscdFuvquaCqx_n_d68BjlaIzjQ5Ht621mztyt0wtQYqYu-h9L9QHNHS5-zzAfTc_1rtFSp_s?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card4Parents.jpg',
     },
 
     {   name: 'gizmo',
-        image: 'https://bl3302files.storage.live.com/y4mlKlCY9eBFZFzhGCH4JZ0um5Ni9RUCw6n1nZX5Qon2a7WMAdghovv3nWxgFhX5tkzDMyjRcOkiyS-MkkMgNXdE-yY4bVhLOBG3afFarPQJufC-c6MAy_0B0erzbOZKMqi4OwQW_-La0xhqFDapCqbFmAX-rSlciiPH5RdEqtzMy_WUTtkPvOyapFPn2OPTKEe?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card3Gizmo.jpg',
     },
 
     {   name: 'gizmo',
-        image: 'https://bl3302files.storage.live.com/y4mlKlCY9eBFZFzhGCH4JZ0um5Ni9RUCw6n1nZX5Qon2a7WMAdghovv3nWxgFhX5tkzDMyjRcOkiyS-MkkMgNXdE-yY4bVhLOBG3afFarPQJufC-c6MAy_0B0erzbOZKMqi4OwQW_-La0xhqFDapCqbFmAX-rSlciiPH5RdEqtzMy_WUTtkPvOyapFPn2OPTKEe?width=300&height=300&cropmode=none',
+        image: 'IMAGES/Card3Gizmo.jpg',
     },
 
 ]
 
+cardsArray.sort(() => 0.5 - Math.random())
+
 // game board
 
-const gameBoard = document.querySelector('.grid')
+const gameBoard = document.querySelector('.gameBoard')
+const resultDisplay = document.querySelectorAll('#result')
+var cardsChosen = []
+var cardsChosenID = []
+var cardsWon = []
 
-function createBoard()
+
+function createBoard() {
+    for (let i = 0; i < cardsArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'IMAGES/Back of Cards.jpg')
+        card.setAttribute('data-id', i)
+
+        card.addEventListener('click', flipCard)
+
+        gameBoard.appendChild(card)
+
+    }
+}
+
+
+/* Check for a match */
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneID = cardsChosenID[0]
+    const optionTwoID = cardsChosenID[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You have a match')
+        cards[optionOneID].setAttribute('src', 'IMAGES/matched.jpg')
+        cards[optionTwoID].setAttribute('src', 'IMAGES/matched.jpg')
+
+        cardsWon.push(cardsChosen)
+    }else {
+        cards[optionOneID].setAttribute('src', 'IMAGES/Back of Cards.jpg')
+        cards[optionTwoID].setAttribute('src', 'IMAGES/Back of Cards.jpg')
+        alert('No match, try again')
+
+    }
+
+    cardsChosen = []
+    cardsChosenID = []
+    resultDisplay.textContent = cardsWon.length 
+    if (cardsWon.length === cardsArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found the most match cards!'
+    }
+
+    }
+
+
+
+}
+
+
+
+
+
+/* Flip Cards over */
+function flipCard() {
+    var cardID = this.getAttribute('data-id')
+    cardsChosen.push(cardsArray[cardID].name)
+    cardsChosenID.push(cardID)
+    this.setAttribute('src', cardsArray[cardID].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+
+    }
+
+}
+
+
+
+
+
+createBoard()
+
+
+
+})
